30bc61a155a96657c9f53b94ddeddf585f3fae9f
haya <hayashi@bi.cs.titech.ac.jp>
2017-05-26 15:37:34 +0900

    [add] homework

--- app/src/main/java/jp/ac/titech/itpro/sdl/gles10ex/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/gles10ex/MainActivity.java
@@ -1,18 +1,34 @@
 package jp.ac.titech.itpro.sdl.gles10ex;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, SensorEventListener  {
     private final static String TAG = "MainActivity";
 
     private GLSurfaceView glView;
     private SimpleRenderer renderer;
     private SeekBar rotationBarX, rotationBarY, rotationBarZ;
+
+    private SensorManager sensorMgr;
+    private Sensor accelerometer;
+
+    private float x = 0, y = 0, z = 0;
+    private float vx, vy, vz;
+    private int accuracy;
+
+    private final static float alpha = 0.75F;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +48,20 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         renderer.addObj(new Cube(0.5f, 0, 0.2f, -3));
         renderer.addObj(new Pyramid(0.5f, 0, 0, 0));
         glView.setRenderer(renderer);
+
+        sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (accelerometer == null) {
+            finish();
+            return;
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Log.d(TAG, "onResume");
+        sensorMgr.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
         glView.onResume();
     }
 
@@ -46,6 +70,7 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         super.onPause();
         Log.d(TAG, "onPause");
         glView.onPause();
+        sensorMgr.unregisterListener(this);
     }
 
     @Override
@@ -66,4 +91,22 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     public void onStopTrackingTouch(SeekBar seekBar) {
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        vx = alpha * vx + (1 - alpha) * event.values[0];
+        vy = alpha * vy + (1 - alpha) * event.values[1];
+        vz = alpha * vz + (1 - alpha) * event.values[2];
+        x += vx;
+        y += vy;
+        z += vz;
+        renderer.setRotationX(x);
+        renderer.setRotationY(y);
+        renderer.setRotationZ(z);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.i(TAG, "onAccuracyChanged: ");
+        this.accuracy = accuracy;
+    }
 }
